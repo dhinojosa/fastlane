bb9a4cb1b98eeb774d1bf00358fbc014f5581836
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2014-01-20 06:44:52 -0600

    added initial source

---
+++ src/main/java/com/etr407/App.java
@@ -0,0 +1,13 @@
+package com.etr407;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
